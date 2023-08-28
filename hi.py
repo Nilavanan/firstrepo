16b099866fbcb3f3ed82ff6c2c384c94408da398
unknown <sanilavanan2330@gmail.com>
2023-08-29 05:24:32 +0530

    we add command changes

--- hi.py
+++ hi.py
@@ -2,7 +2,17 @@ print("hello world")
 
 
 a=1
-b=2
-print(a+b)  # git add
+b=200 
+print(a+b)  # git add  .\<filename>
 
 # git status  -  modified files
+
+ # git config --global user.email "you@example.com"   or   git config --global user.name "Your Name"
+
+a=12
+b=23
+c=100
+print(c+b+a)
+
+# git commit -m "msg"
+
